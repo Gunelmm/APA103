e1d8d41c308ddd2fae4bdedfb975e0109da34c97
Gunelmm <gunalhm-apa103@code.edu.az>
2026-03-15 21:48:09 +0400

    Yazdığım kodu əlavə etdim

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = { 1, 4, 9, 16, 25 };
+int[] additionalNum = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+int[] newArray = ArrayResize(numbers, additionalNum);
+Console.WriteLine(string.Join(" ",  newArray));
+
+static int[] ArrayResize(int[] numbers, params int[] additionalNum)
+{
+    int newLength = numbers.Length + additionalNum.Length;
+    int[] newArray = new int[newLength];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        newArray[i] = numbers[i];
+    }
+    for (int i=0; i<additionalNum.Length; i++)
+    {
+        newArray[numbers.Length+i] = additionalNum[i];
+    }
+    return newArray;
+}
