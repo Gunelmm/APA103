88582ab110824efa9cb3cd0beb4717453afed24c
Gunelmm <gunalhm-apa103@code.edu.az>
2026-05-13 06:55:43 +0400

    mvc task

--- 27-FrontToBackSqlConnection/Program.cs
+++ 27-FrontToBackSqlConnection/Program.cs
@@ -14,9 +14,13 @@ var app = builder.Build();
 app.UseStaticFiles();
 
 app.MapControllerRoute(
+    name: "admin",
+    pattern: "{area:exists}/{controller=dashboard}/{action=Index}/{id?}");
+
+
+app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
